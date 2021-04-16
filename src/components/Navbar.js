e458b476c609d428cf89d07d508195d763ee8934
Albert Chang <a89529294@hotmail.com>
2021-04-16 15:00:22 +0800

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,18 +8,32 @@ import close from '../assets/icons/close.svg';
 export default function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
 
+  const toggleMenu = () => {
+    setShowMenu((showMenu) => !showMenu);
+  };
+
   return (
     <div className="relative flex items-center justify-between p-8 pb-10">
       <Link to="/">
         <img src={logo} alt="logo" />
       </Link>
 
-      <img
-        src={showMenu ? close : hamburger}
-        alt="hamburger menu"
-        className={`${showMenu ? 'w-5 h-5' : 'w-6 h-3'}`}
-        onClick={() => setShowMenu((showMenu) => !showMenu)}
-      />
+      {showMenu ? (
+        <img
+          src={close}
+          alt="close icon"
+          className="w-5 h-5"
+          onClick={toggleMenu}
+        />
+      ) : (
+        <img
+          src={hamburger}
+          className="w-6 h-3"
+          alt="hamburger icon"
+          onClick={toggleMenu}
+        />
+      )}
+
       {showMenu && (
         <div className="absolute w-56 py-10 text-center text-white top-20 bg-gray-dark-blue right-8 btn-label">
           <h1 className="mb-8" onClick={() => setShowMenu(false)}>
