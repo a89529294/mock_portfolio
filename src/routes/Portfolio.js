79a8613813bb4ddccacbe67d8fda2531ff9d26c6
Albert Chang <a89529294@hotmail.com>
2021-04-18 17:26:38 +0800

    .

--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -1,15 +1,22 @@
+import useMediaQuery from '../hooks/useMediaQuery';
+
 import manageImage from '../assets/portfolio/mobile/image-portfolio-manage@2x.jpg';
 import bookmarkImage from '../assets/portfolio/mobile/image-portfolio-bookmark@2x.jpg';
 import insureImage from '../assets/portfolio/mobile/image-portfolio-insure@2x.jpg';
 import fyloImage from '../assets/portfolio/mobile/image-portfolio-fylo@2x.jpg';
+import manageImageLarge from '../assets/portfolio/desktop/image-portfolio-manage@2x.jpg';
+import bookmarkImageLarge from '../assets/portfolio/desktop/image-portfolio-bookmark@2x.jpg';
+import insureImageLarge from '../assets/portfolio/desktop/image-portfolio-insure@2x.jpg';
+import fyloImageLarge from '../assets/portfolio/desktop/image-portfolio-fylo@2x.jpg';
 
 import SectionWithImage from '../components/SectionWithImage';
 
 export default function Porfolio() {
+  const isLargerThanMobile = useMediaQuery('(min-width: 640px)');
   return (
     <div className="p-8 pt-0 space-y-20">
       <SectionWithImage
-        image={manageImage}
+        image={isLargerThanMobile ? manageImageLarge : manageImage}
         title="Manage"
         body={`This project required me to build a fully responsive landing page to the designs provided. 
         I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, 
@@ -17,11 +24,12 @@ export default function Porfolio() {
         btnLabel="VIEW PROJECT"
         linkURL="/portfolio/manage"
         widthsForTablet={['w-1/2', 'w-3/8']}
+        widthsForDesktop={['w-1/2', 'w-3/8']}
         imageYPadding={true}
         lineupImageBottomWithButton={true}
       />
       <SectionWithImage
-        image={bookmarkImage}
+        image={isLargerThanMobile ? bookmarkImageLarge : bookmarkImage}
         title="Bookmark"
         body={`This project required me to build a fully responsive landing page to the designs provided. 
         I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, 
@@ -29,12 +37,13 @@ export default function Porfolio() {
         btnLabel="VIEW PROJECT"
         linkURL="/portfolio/bookmark"
         widthsForTablet={['w-1/2', 'w-3/8']}
+        widthsForDesktop={['w-1/2', 'w-3/8']}
         imageYPadding={true}
         lineupImageBottomWithButton={true}
         swap={true}
       />
       <SectionWithImage
-        image={insureImage}
+        image={isLargerThanMobile ? insureImageLarge : insureImage}
         title="Insure"
         body={`This was a small project which mostly consisted of HTML and CSS. 
           I built a fully-responsive landing page. 
@@ -42,11 +51,12 @@ export default function Porfolio() {
         btnLabel="VIEW PROJECT"
         linkURL="/portfolio/insure"
         widthsForTablet={['w-1/2', 'w-3/8']}
+        widthsForDesktop={['w-1/2', 'w-3/8']}
         imageYPadding={true}
         lineupImageBottomWithButton={true}
       />
       <SectionWithImage
-        image={fyloImage}
+        image={isLargerThanMobile ? fyloImageLarge : fyloImage}
         title="Fylo"
         body={`This project was built in pure HTML and CSS. 
           I had mobile and desktop designs to work to and built it so that it was fully-responsive. 
@@ -54,6 +64,7 @@ export default function Porfolio() {
         btnLabel="VIEW PROJECT"
         linkURL="/portfolio/fylo"
         widthsForTablet={['w-1/2', 'w-3/8']}
+        widthsForDesktop={['w-1/2', 'w-3/8']}
         imageYPadding={true}
         lineupImageBottomWithButton={true}
         swap={true}
